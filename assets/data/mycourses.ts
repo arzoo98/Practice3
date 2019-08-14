0ac8ab5b89ef9136f1ec43c51f96df6d0a1c8b59
arzoo98 <52541972+arzoo98@users.noreply.github.com>
2019-08-13 23:47:34 -0400

    Add files via upload

---
+++ assets/data/mycourses.ts
@@ -0,0 +1,6 @@
+import { Course } from '../../app/courses';
+
+export const MYCOURSES: Course[] = [
+    {cid: 'SYST2444', cname: 'Mobile web', cprof: 'Andy Pak'},
+    {cid: 'SYST2432', cname: 'PMP', cprof: 'Vidya Narayan'},
+]; 
